794f6c40c382e5155682999f2ff054cc7c3e17ad
fasma <dingaanmanjate@gmail.com>
2026-02-18 12:12:50 +0200

    First variant, can calculate the first root

--- main.cpp
+++ main.cpp
@@ -1,10 +1,41 @@
 #include <iostream>
 #include <string>
+#include <cmath>
+using namespace std;
+
+float equation(float x) {
+    float result = pow(x, 3) + 2*x - 2; // Example equation: f(x) = x^3 + 2x - 2
+    return result;
+}
+float bisection(float &a, float &b, float p) {
+    if (equation(a)*equation(b)>0){
+        cout<< a << ":" << b <<" are out of range";
+    }
+    else if (equation(p) == 0) {
+        cout << p << " is the root";
+    }
+    else if (equation(p)<0) {
+        a = p; 
+    }
+    else {
+        b = p;
+    }
+    return (a + b)/2;
+}
+float iteration(float &a, float &b){
+    float p, pnext;
+    do {
+        p = (a + b)/2;
+        pnext = bisection(a, b, p);
+    }while (abs(p-pnext)/abs(min(a,b))>pow(10,-4));
+    return p;
+}
+
 
 int main() {
-    std::string name;
-    std::cout << "Enter your name: ";
-    std::getline(std::cin, name);
-    std::cout << "Hello, " << name << "!" << std::endl;
-    return 0;
+    float a, b;
+    cout << "Enter the initial values to consider delimated by 'ENTER'"<<endl;
+    cin>>a>>b;
+
+    cout << iteration(a, b);
 }
---
+++ readme.md
@@ -0,0 +1,7 @@
+Build the files, then run 
+'''sh
+./main
+'''
+from there input the min number in the range
+press enter
+input the max number in the range
